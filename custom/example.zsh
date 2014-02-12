7ae8405d8511bf923f483b18fdb26c937b4e9e9f
Vishwa Chandra Padaraju <vishwachandra.padaraju@cerner.com>
2014-02-12 09:58:27 -0600

    Custom commands.

--- custom/example.zsh
+++ custom/example.zsh
@@ -2,4 +2,15 @@
 # Example:
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
+
+#Shortcuts:
+ws=~/Documents/workspace
+
+#aliases:
+alias mvnInst='mvn clean install'
+alias mvnTest='mvn clean test'
+alias mvnInstSkip='mvn clean install -DskipTests'
+alias mvnPullTest='mvn clean verify -gs /Users/VP027915/.m2/nightlySettings.xml'
+
+#exports:
+export MAVEN_OPTS="-XX:MaxPermSize=256m -Xmx1024m -d32"
