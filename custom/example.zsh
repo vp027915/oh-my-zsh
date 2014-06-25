d69ce6e1dce2119ab045623e9361d41e846c5e02
Vishwa Chandra Padaraju <vishwachandra.padaraju@cerner.com>
2014-06-25 09:59:37 -0500

    Java home set.

--- custom/example.zsh
+++ custom/example.zsh
@@ -14,3 +14,4 @@ alias mvnPullTest='mvn clean verify -gs /Users/VP027915/.m2/nightlySettings.xml'
 
 #exports:
 export MAVEN_OPTS="-XX:MaxPermSize=256m -Xmx1024m -d32"
+export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home; echo \"Java 1.6 set.\"
